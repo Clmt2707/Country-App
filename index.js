e1a2aafebd34fc2eff51f55942329a6c11241734
Clmt2707 <clement.deboeuf@gmail.com>
2024-09-03 16:04:13 +0200

    Fonction d"affichage des pays

--- index.js
+++ index.js
@@ -6,22 +6,25 @@ async function getCountry () {
     await fetch('https://restcountries.com/v3.1/all')
         .then((res) => res.json())
         .then((data) => {
-            countriesData = data;
-            
+            countriesData = data;           
         }) 
         console.log(countriesData);
+        
         countriesDisplay();
 }
 
+
 function countriesDisplay () {
     countriesContainer.innerHTML = countriesData.map((country) =>
+        `
+               <div class="card">
+                    <img src=${country.flags.svg} alt="Drapeau ${country.name.common}" >  
+                    <h2>${country.translations.fra.common}</h2>
+                    <h4>${country.capital}</h4>
+                </div>
             `
-            <div class="card">
-                <h2>${country.translations.fra.common}</h2>
-                <img src="${country.flags.svg}" alt="Drapeau ${country.name.common}></img>
-            </div>
-            `
-    );
+        ).join('');
 }
 
-window.addEventListener('load', getCountry)
+
+window.addEventListener("load", getCountry);
